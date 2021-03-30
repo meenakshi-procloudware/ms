e6a6e15ba224f6fb4f17e866d90432491a654c1a
meenakshi-procloudware <meenakshi.procloudware@gmail.com>
2021-03-30 11:44:50 +0530

    modify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ms';
-  fname:string = '';
-  lname:any;
-  age:any;
-  gender:any;
-  contact:any;
-  email:any;
-  // fnameError:boolean;
-  nextClick(){
-    console.log("fname",this.fname);
-    if(this.fname=== ''|| this.fname === undefined){
-    // this.fnameError = true;
+  fname: string = '';
+  lname: any;
+  age: any;
+  gender: any;
+  contact: any;
+  Email: any;
+  fnameError: boolean = false;
+  lnameError: boolean = false;
+  ageError: boolean = false;
+  genderError: boolean = false;
+  contactError: boolean = false;
+  EmailError: boolean=false;
+
+  nextClick() {
+    if (this.fname === '' || this.fname === undefined) {
+      this.fnameError = true;
     }
+    if (this.lname === '' || this.lname === undefined) {
+      this.lnameError = true;
+    }
+    if (this.age === '' || this.age === undefined) {
+      this.ageError = true;
+    }
+    if (this.gender === '' || this.gender === undefined) {
+      this.genderError = true;
+    }
+    if (this.contact === '' || this.contact === undefined) {
+      this.contactError = true;
+    }
+    if (this.Email === '' || this.Email === undefined) {
+      this.EmailError = true;
+    }
+
+
   }
-  
+
 }
-
-
